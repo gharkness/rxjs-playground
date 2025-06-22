cd2133ebed46e1475f9fc7c31c144f537f23755f
Garrett Harkness <gharkness@sourceallies.com>
2025-06-22 14:09:57 -0500

    next notification

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Observable } from 'rxjs';
 
-const observable$ = new Observable(subscriber => {
-    console.log('Observable executed')
+const observable$ = new Observable<string>(subscriber => {
+    console.log('Observable executed');
+    subscriber.next('Alice');
 });
 
 console.log('Before subscribe');
-observable$.subscribe();
+observable$.subscribe(value => console.log(value));
 console.log('After subscribe');
